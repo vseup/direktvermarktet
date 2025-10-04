a00a62f4e54a88ff3acf6947aab0407371fc3333
vseup <hello@verenapues.com>
2025-10-04 20:45:26 +0200

    chore: added prisma and docker-compose.yml

---
+++ scripts/dev.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+echo "🐳 starting dev database docker container..."
+docker compose up -d
+
+echo "🚀 starting frontend and backend..."
+concurrently -r \
+  "npm run start:dev --workspace=direktvermarktet-api" \
+  "npm run dev --workspace=direktvermarktet-client"
+
+echo "🧹 stopping docker containers..."
+docker compose down
