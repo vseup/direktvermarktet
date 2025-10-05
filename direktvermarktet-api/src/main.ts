bd5ee6ce4a8e3620616573c6583c2caf500ce2ac
vseup <hello@verenapues.com>
2025-10-05 22:45:28 +0200

    feat(cors): enable cors

--- direktvermarktet-api/src/main.ts
+++ direktvermarktet-api/src/main.ts
@@ -3,11 +3,19 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ 
-    whitelist: true, 
-    forbidNonWhitelisted: true 
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true
   }));
+
+  app.enableCors({
+    origin: [process.env.CLIENT_URL],
+    credentials: true
+  })
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
