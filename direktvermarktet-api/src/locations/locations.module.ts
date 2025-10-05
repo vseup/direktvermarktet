eb2c51643295f2633c3314a65f2a658b8bab788a
vseup <hello@verenapues.com>
2025-10-05 16:36:59 +0200

    chore: deleted unused locations controller and service

--- direktvermarktet-api/src/locations/locations.module.ts
+++ direktvermarktet-api/src/locations/locations.module.ts
@@ -1,9 +1,4 @@
 import { Module } from '@nestjs/common';
-import { LocationsService } from './locations.service';
-import { LocationsController } from './locations.controller';
 
-@Module({
-  controllers: [LocationsController],
-  providers: [LocationsService],
-})
-export class LocationsModule {}
+@Module({})
+export class LocationsModule { }
