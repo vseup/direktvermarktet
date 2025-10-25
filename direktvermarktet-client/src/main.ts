07903c49cd6f693dd19a71c688e6d1c8be1bfd2b
vseup <hello@verenapues.com>
2025-10-25 15:20:02 +0200

    feat(farmshop-gallery): connect client to api

--- direktvermarktet-client/src/main.ts
+++ direktvermarktet-client/src/main.ts
@@ -6,6 +6,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import axios from 'axios'
+
+export const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL
+})
+
 const app = createApp(App)
 
 app.use(createPinia())
