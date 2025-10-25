4a8febe7c1762937457510a054e952ff608ddea2
vseup <hello@verenapues.com>
2025-10-25 14:35:01 +0200

    feat(validation): use zod validation pipe

--- direktvermarktet-api/src/main.ts
+++ direktvermarktet-api/src/main.ts
@@ -1,15 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ZodValidationPipe } from 'nestjs-zod';
 
 async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true
-  }));
+  app.useGlobalPipes(new ZodValidationPipe());
 
   app.enableCors({
     origin: [process.env.CLIENT_URL],
@@ -18,4 +15,5 @@ async function bootstrap() {
 
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
